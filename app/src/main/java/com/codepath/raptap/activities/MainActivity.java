869c475b02bb9e56b815e33656d3e1a7e4dc9f36
Jack Walter Yurkanin <yurkanin@fb.com>
2021-07-19 15:57:28 -0700

    Added menu items logout and Studio (for making recording)

--- app/src/main/java/com/codepath/raptap/activities/MainActivity.java
+++ app/src/main/java/com/codepath/raptap/activities/MainActivity.java
@@ -2,10 +2,15 @@ package com.codepath.raptap.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.codepath.raptap.R;
 import com.codepath.raptap.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miLogout:
+                ParseUser.logOut();
+                if(ParseUser.getCurrentUser() == null)
+                    goToLoginActivity();
+                return true;
+            case R.id.miStudio:
+                Intent studio = new Intent(MainActivity.this, SoundActivity.class);
+                startActivity(studio);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goToLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
