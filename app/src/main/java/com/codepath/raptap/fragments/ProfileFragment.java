de387c898d33186f46b6d52a8755e1e8d189df2a
Jack Walter Yurkanin <yurkanin@fb.com>
2021-08-05 16:10:07 -0700

    Populated profile page

--- app/src/main/java/com/codepath/raptap/fragments/ProfileFragment.java
+++ app/src/main/java/com/codepath/raptap/fragments/ProfileFragment.java
@@ -7,28 +7,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.codepath.raptap.databinding.FragmentProfileBinding;
 import com.codepath.raptap.models.User;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class ProfileFragment extends Fragment {
+import org.json.JSONArray;
 
+public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
+    private static final String KEY_BIO = "bio";
+    private static final String KEY_LIBRARY = "library";
+    private static final String KEY_PROFILE_PICTURE = "picture";
     private Context context;
     private FragmentProfileBinding binding;
     private TextView tvBio;
     private TextView tvUsername;
     private ImageView ivProfilePic;
-    private User user;
+    private ParseUser user;
     private ParseFile profileImage;
+    private String username;
+    private String bio;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -51,20 +62,6 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(getLayoutInflater(), container, false);
         View v = binding.getRoot();
-
-        tvBio = binding.tvBio;
-        tvUsername = binding.tvProfileHandle;
-        ivProfilePic = binding.ivProfilePic;
-
-        user = (User) ParseUser.getCurrentUser();
-        tvBio.setText(user.getBio());
-        tvUsername.setText(user.getUserName());
-        profileImage = user.getProfilePic();
-
-        if (profileImage != null){
-            Glide.with(context).load(profileImage.getUrl()).circleCrop().into(ivProfilePic);
-        }
-
         return v;
     }
 
@@ -72,5 +69,42 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tvBio = binding.tvBio;
+        tvUsername = binding.tvUsername;
+        ivProfilePic = binding.ivProfilePic;
+
+        user = ParseUser.getCurrentUser();
+        username = user.getUsername();
+        getUserFields();
+
+        tvUsername.setText(username);
+
+        JSONArray library = user.getJSONArray(KEY_LIBRARY);
+        // ToDo:
+        //   need to add recycler function for songs in library
+    }
+
+    public void getUserFields() {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("username", username);
+        query.findInBackground((users, e) -> {
+            if (e == null) {
+                // The query was successful, returns the users that matches
+                // the criteria.
+                for(ParseUser user1 : users) {
+                    Log.d("User List ", (user1.getUsername()));
+                    bio = user1.getString(KEY_BIO);
+                    tvBio.setText(bio);
+                    profileImage = user1.getParseFile(KEY_PROFILE_PICTURE);
+                    Log.e(TAG, String.valueOf(profileImage));
+                    if (profileImage != null){
+                        Glide.with(context).load(profileImage.getUrl()).circleCrop().into(ivProfilePic);
+                    }
+                }
+            } else {
+                // Something went wrong.
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
